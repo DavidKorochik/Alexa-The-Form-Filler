cc9de8614bae0094f3eaacae6309b9fc2e055f3d
DavidKorochik <david1korochik@gmail.com>
2021-09-25 12:20:15 +0300

    Added a proxy error checker function to the utils folder

--- ausettlerForm.js
+++ ausettlerForm.js
@@ -4,9 +4,11 @@ const Views = require('./models/views');
 const db = require('./db/mongoose');
 const viewsReset = require('./utils/viewsReset');
 const googleSheetsFiller = require('./utils/googleSheets');
+const fvisionCheck = require('./utils/fvisionCheck');
 const openSites = require('./utils/multipleSites');
 const current = require('./utils/currentTime');
 const pageView = require('./utils/pageView');
+const fvisionCheck = require('./utils/fvisionCheck');
 require('dotenv').config();
 
 (async () => {
@@ -33,21 +35,27 @@ require('dotenv').config();
 
   page.setDefaultNavigationTimeout(0);
 
+  await page.setViewport({ width: 1536, height: 864 }); // Changing the resolution of the chrome tab
+
   // Proxy authentication
   // page.authenticate({ username, password });
 
+  // Navigating to the f.vision site to check if there is any proxy error, and if there is we exit the bot and if there is no proxy error we proceed with the form filler bot
+  // await fvisionCheck(page);
+
   // Condition that handles the deletion of the views document and inserts at 12AM the summary of the views
   if (current === '00:00:00' || current === '24:00:00') {
+    //Executing the views reset function @ ./utils/viewsRest.js
     await viewsReset();
   }
 
-  // Visit all sites synchronously
+  // Visit all sites synchronously @ ./utils/multipleSites.js
   await openSites(browser);
 
   // Navigating to the form
   await page.goto('http://ausettler.com/test/form.php');
 
-  // Executing the page view function
+  // Executing the page view function @ ./utils/pageViews.js
   await pageView(browser);
 
   // Counting the views each execution
@@ -60,8 +68,9 @@ require('dotenv').config();
   // Typing the zip code in the form
   await page.type('#zipcode', zip);
 
+  // Pressing the Enter button to continue to the form filler
   await page.keyboard.press('Enter');
 
-  // Filling the form with the data from the google sheet and inserting the SUCCESS or FAIL word due to the registration
+  // Filling the form with the data from the google sheet and inserting the SUCCESS or FAIL word due to the registration @ ./utils/googleSheets.js
   await googleSheetsFiller(page);
 })().catch((err) => console.log(err));
---
+++ utils/fvisionCheck.js
@@ -0,0 +1,19 @@
+const fvisionCheck = async (page) => {
+  await page.goto('http://f.vision/'); // Navigating to the f.vision site to check if there is any proxy error
+
+  await page.$eval('#start-button', (submit) => submit.click()); // Clicking on the start button in the bottom to start the checking
+
+  await page.waitForSelector('.danger');
+
+  const el = await page.$('[data-func="anonymizer_type"]');
+  const className = await (await el.getProperty('className')).jsonValue();
+
+  // Navigating to the liberty site
+  className == 'col-sm-6 col-xs-6 nav-link nav-parts danger'
+    ? process.exit(1)
+    : await page.goto('https://www.libertyhomeguard.com/', {
+        waitUntil: 'networkidle2',
+      });
+};
+
+module.exports = fvisionCheck;
